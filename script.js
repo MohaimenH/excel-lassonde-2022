128e8f9806d736fb661bcb3cb812166754c61597
Ammar Mohieldin <ammarmo@my.yorku.ca>
2023-07-09 20:41:32 -0400

    Everything

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function openPopUp(a){
+    let popup = document.getElementById("popup".concat(a));
+    let previous = document.getElementsByClassName("open-popup");
+
+    
+    if(previous.length > 0) { //checks if there is more than 1 popup
+        previous.item(0).classList.remove("open-popup"); //closes previous popup
+    }
+
+    popup.classList.add("open-popup"); //open (new) popup by making it visible
+}
+
+function closePopUp(a){
+    let popup = document.getElementById("popup".concat(a));
+    popup.classList.remove("open-popup"); //remove popup by removing visibility
+}
